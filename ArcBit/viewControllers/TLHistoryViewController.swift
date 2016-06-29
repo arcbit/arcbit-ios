b959917b1f57de682c836d2c32be7b413c9f561b
stequald <stequald@gmail.com>
2016-06-29 13:18:05 -0700

    Change back to red color

--- ArcBit/viewControllers/TLHistoryViewController.swift
+++ ArcBit/viewControllers/TLHistoryViewController.swift
@@ -234,7 +234,7 @@ import CoreData
         cell!.descriptionLabel!.adjustsFontSizeToFitWidth = true
         if (amountType == .Send) {
             amountTypeString = "-"
-            cell!.amountButton!.backgroundColor = UIColor.init(colorLiteralRed: 254/255.0, green: 46/255.0, blue: 46/255.0, alpha: 1)
+            cell!.amountButton!.backgroundColor = UIColor.redColor()
             if txTag == nil || txTag == "" {
                 let outputAddressToValueArray = txObject!.getOutputAddressToValueArray()
                 for _dict in outputAddressToValueArray! {
